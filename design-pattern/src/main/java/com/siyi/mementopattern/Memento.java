807f90471c99dd71d5f7a80c3091231375147156
私忆一秒钟 <siyiyimiaozhong@qq.com>
2021-02-09 23:06:49 +0800

    （二十）备忘录模式（Memento Pattern）

---
+++ design-pattern/src/main/java/com/siyi/mementopattern/Memento.java
@@ -0,0 +1,13 @@
+package com.siyi.mementopattern;
+
+public class Memento {
+   private String state;
+ 
+   public Memento(String state){
+      this.state = state;
+   }
+ 
+   public String getState(){
+      return state;
+   }  
+}
